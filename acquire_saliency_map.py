331b974a170192b9ff99f3168e95f3ee51decb80
isl-kobayan <adpa0179@chiba-u.jp>
2017-01-27 16:23:14 +0900

    add saliency map acquirer

---
+++ acquire_saliency_map.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+"""Train convnet for MINC-2500 dataset.
+
+Prerequisite: To run this example, crop the center of ILSVRC2012 training and
+validation images and scale them to 256x256, and make two lists of space-
+separated CSV whose first column is full path to image and second column is
+zero-origin label (this format is same as that used by Caffe's ImageDataLayer).
+
+"""
+from __future__ import print_function
+import argparse
+import random
+
+import numpy as np
+
+import chainer
+from chainer import training
+from chainer.training import extensions
+from chainer import cuda
+import models
+import utils
+import preprocessed_dataset as ppds
+import datetime
+import time
+import os
+
+image_size = 362
+
+def makeMeanImage(mean_value):
+    mean_image = np.ndarray((3, image_size, image_size), dtype=np.float32)
+    for i in range(3): mean_image[i] = mean_value[i]
+    return mean_image
+
+def main(args):
+    chainer.set_debug(True)
+    # Initialize the model to train
+    model = models.archs[args.arch]()
+    if args.finetune and hasattr(model, 'finetuned_model_path'):
+        utils.finetuning.load_param(model.finetuned_model_path, model, args.ignore)
+    if args.initmodel:
+        print('Load model from', args.initmodel)
+        chainer.serializers.load_npz(args.initmodel, model)
+    if args.gpu >= 0:
+        chainer.cuda.get_device(args.gpu).use()  # Make the GPU current
+        #if args.test:
+        #cuda.cudnn_enabled = False
+        model.to_gpu()
+
+    nowt = datetime.datetime.today()
+    outputdir = os.path.join(args.out, args.arch, 'extract')
+    if args.initmodel is not None:
+        outputdir = os.path.dirname(args.initmodel)
+        #if args.indices is None:
+        #    args.indices = os.path.join(outputdir, 'features', 'top_' + args.layer + '.txt')
+    # Load the datasets and mean file
+    mean = None
+    if hasattr(model, 'mean_value'):
+        mean = makeMeanImage(model.mean_value)
+    else:
+        mean = np.load(args.mean)
+    assert mean is not None
+
+    #if args.indices is None:
+    #    args.indices = os.path.join(args.out, args.arch, 'extract', 'top_' + args.layer + '.txt')
+
+    #top_path = os.path.join(args.out, args.arch, 'extract', 'top_' + args.layer + '.txt')
+    #train = ppds.PreprocessedDataset(args.train, args.root, mean, model.insize)
+    val = ppds.PreprocessedDataset(args.val, args.root, mean, model.insize, False)
+    # These iterators load the images with subprocesses running in parallel to
+    # the training/validation.
+    #train_iter = chainer.iterators.MultiprocessIterator(
+    #    train, args.batchsize, shuffle=False, n_processes=args.loaderjob)
+    #val_iter = chainer.iterators.MultiprocessIterator(
+    #    val, args.val_batchsize, repeat=False, shuffle=False, n_processes=args.loaderjob)
+    val_iter = chainer.iterators.SerialIterator(
+            val, args.val_batchsize, repeat=False, shuffle=False)
+
+    # Set up an optimizer
+    optimizer = chainer.optimizers.MomentumSGD()
+    optimizer.setup(model)
+
+    # Set up a trainer
+    updater = training.StandardUpdater(val_iter, optimizer, device=args.gpu)
+    trainer = training.Trainer(updater, (1, 'epoch'), outputdir)
+
+    #val_interval = (10 if args.test else int(len(train) / args.batchsize)), 'iteration'
+    val_interval = (1, 'iteration')
+    #snapshot_interval = (10, 'iteration') if args.test else (2, 'epoch')
+    log_interval = (10, 'iteration')
+
+    # Copy the chain with shared parameters to flip 'train' flag only in test
+    eval_model = model.copy()
+    eval_model.train = False
+    val_acquirer = utils.SaliencyMapAcquirer(val_iter, eval_model, device=args.gpu)
+    val_acquirer.mean = mean
+    val_acquirer.layer_rank = eval_model.layer_rank[args.layer]
+    val_acquirer.layer_name = args.layer
+    val_acquirer.operation = args.operation
+    val_acquirer.fixed_RMS = args.rms
+    #val_acquirer.top = args.top
+    val_acquirer.target = args.target
+    val_acquirer.n_features = model.labelsize
+    if 'googlenet' in args.arch:
+        val_acquirer.lastname = 'validation/main/loss3'
+    trainer.extend(val_acquirer, trigger=val_interval)
+    #trainer.extend(extensions.dump_graph('main/loss'))
+    #trainer.extend(extensions.snapshot(), trigger=snapshot_interval)
+    #trainer.extend(extensions.snapshot_object(
+    #    model, 'model_iter_{.updater.iteration}'), trigger=snapshot_interval)
+    # Be careful to pass the interval directly to LogReport
+    # (it determines when to emit log rather than when to read observations)
+    #trainer.extend(extensions.LogReport(trigger=log_interval))
+    trainer.extend(extensions.PrintReport([
+        'epoch', 'iteration', 'main/loss', 'validation/main/loss',
+        'main/accuracy', 'validation/main/accuracy',
+    ]), trigger=log_interval)
+    trainer.extend(extensions.ProgressBar(update_interval=10))
+    #trainer.extend(extensions.ExponentialShift('lr', args.gamma),
+    #    trigger=(1, 'epoch'))
+
+    if args.resume:
+        chainer.serializers.load_npz(args.resume, trainer)
+
+    #if not args.test:
+    #    trainer.run()
+    #    chainer.serializers.save_npz(outputdir + '/model', model)
+
+    results = val_acquirer(trainer)
+    results['outputdir'] = outputdir
+
+    #if eval_model.layer_rank[args.layer] == 1:
+    #    save_first_conv_filter(os.path.join(outputdir, args.layer),
+    #    model[args.layer].W.data, cols = args.cols, pad = args.pad,
+    #    scale = args.scale, gamma = args.gamma)
+
+    #if args.test:
+    #print(val_acquirer.confmat)
+    #categories = utils.io.load_categories(args.categories)
+    #confmat_csv_name = args.initmodel + '.csv'
+    #confmat_fig_name = args.initmodel + '.eps'
+    #utils.io.save_confmat_csv(confmat_csv_name, val_acquirer.confmat, categories)
+    #utils.io.save_confmat_fig(confmat_fig_name, val_acquirer.confmat, categories,
+    #                        mode="rate", saveFormat="eps")
+    return results
+
+parser = argparse.ArgumentParser(
+    description='Learning convnet from MINC-2500 dataset')
+#parser.add_argument('train', help='Path to training image-label list file')
+parser.add_argument('val', help='Path to validation image-label list file')
+parser.add_argument('--categories', '-c', default='categories.txt',
+                    help='Path to category list file')
+parser.add_argument('--arch', '-a', choices=models.archs.keys(), default='nin',
+                    help='Convnet architecture')
+#parser.add_argument('--batchsize', '-B', type=int, default=32,
+#                    help='Learning minibatch size')
+#parser.add_argument('--baselr', default=0.001, type=float,
+#                    help='Base learning rate')
+#parser.add_argument('--gamma', default=0.7, type=float,
+#                    help='Base learning rate')
+#parser.add_argument('--epoch', '-E', type=int, default=10,
+#                    help='Number of epochs to train')
+parser.add_argument('--layer', '-l', default='conv1',
+                    help='layer name')
+parser.add_argument('--indices', '-i',
+                    help='indices file name (e.g. top_conv1.txt)')
+parser.add_argument('--operation', '-op', choices=('max', 'mean'), default='max',
+                    help='operation')
+parser.add_argument('--scale', '-s', type=int, default=1,
+                    help='filter scale')
+parser.add_argument('--pad', '-p', type=int, default=1,
+                    help='filter padding')
+parser.add_argument('--cols', type=int, default=1,
+                    help='columns')
+parser.add_argument('--gamma', '-G', type=float, default=1.0,
+                    help='gamma')
+parser.add_argument('--rms', type=float, default=0.02,
+                    help='rms')
+parser.add_argument('--target', '-t', default='pred', choices=('pred', 'truth'),
+                    help='get saliency map from pred/truth neuron')
+parser.add_argument('--gpu', '-g', type=int, default=-1,
+                    help='GPU ID (negative value indicates CPU)')
+parser.add_argument('--finetune', '-f', default=False, action='store_true',
+                    help='do fine-tuning if this flag is set (default: False)')
+parser.add_argument('--initmodel',
+                    help='Initialize the model from given file')
+parser.add_argument('--ignore', nargs='*', default=[],
+                    help='Ignored layers in parameter copy')
+parser.add_argument('--loaderjob', '-j', type=int,
+                    help='Number of parallel data loading processes')
+parser.add_argument('--mean', '-m', default='mean.npy',
+                    help='Mean file (computed by compute_mean.py)')
+parser.add_argument('--resume', '-r', default='',
+                    help='Initialize the trainer from given file')
+parser.add_argument('--out', '-o', default='result',
+                    help='Output directory')
+parser.add_argument('--root', '-R', default='.',
+                    help='Root directory path of image files')
+parser.add_argument('--val_batchsize', '-b', type=int, default=20,
+                    help='Validation minibatch size')
+#parser.add_argument('--test', action='store_true')
+#parser.set_defaults(test=False)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    val_result = main(args)
+    print('loss\taccuracy')
+    print(str(val_result['validation/main/loss']) + '\t' + str(val_result['validation/main/accuracy']))
---
+++ utils/saliencymapacquirer.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import copy
+
+import six
+
+import chainer
+from chainer.training import extensions
+from chainer.dataset import convert
+from chainer.dataset import iterator as iterator_module
+from chainer import link
+from chainer import reporter as reporter_module
+from chainer.training import extension
+from chainer import variable
+from chainer import cuda
+import numpy as np
+import cupy
+import os
+import csv
+import chainer.functions as F
+import ioutil
+import variableutil as Vutil
+import math
+
+class SaliencyMapAcquirer(extensions.Evaluator):
+    lastname = 'validation/main/loss'
+    layer_rank = None
+    layer_name = None
+    operation = 'max'
+    n_features = None
+    mean = None
+    switch_1stlayer = False
+    guided = True
+    ignore_bias = True
+    fixed_RMS = 0.020
+    rms_axis = 0
+    target='pred'
+    gamma = 1.0
+
+
+    '''trigger = 1, 'epoch'
+    default_name = 'validation'
+    priority = extension.PRIORITY_WRITER
+
+    def __init__(self, iterator, target, converter=convert.concat_examples,
+                 device=None, eval_hook=None, eval_func=None):
+        if isinstance(iterator, iterator_module.Iterator):
+            iterator = {'main': iterator}
+        self._iterators = iterator
+
+        if isinstance(target, link.Link):
+            target = {'main': target}
+        self._targets = target
+
+        self.converter = converter
+        self.device = device
+        self.eval_hook = eval_hook
+        self.eval_func = eval_func'''
+
+    def getpred(self, variable):
+        xp = cuda.get_array_module(variable.data)
+        return xp.argmax(variable.data, axis=1)
+
+    def deconv(self, variable):
+        v = variable
+        # 今の層から入力層に辿り着くまで繰り返す
+        while (v.creator is not None):
+            bottom_blob = v.creator.inputs[0]
+            print(v.creator.label, v.rank)
+            # Convolution -> Deconvolution
+            if (v.creator.label == 'Convolution2DFunction'):
+                bottom_blob.data = Vutil.invert_convolution(v,
+                    guided=self.guided, ignore_bias=self.ignore_bias,
+                    rms=self.fixed_RMS, gamma=self.gamma)
+            # relu -> relu
+            elif (v.creator.label == 'ReLU'):
+                bottom_blob.data = Vutil.invert_relu(v)
+            # Pooling -> UnPooling
+            elif (v.creator.label == 'MaxPooling2D'):
+                bottom_blob.data = Vutil.invert_maxpooling(v, guided=self.guided)
+            # Fully-connected: transpose
+            elif (v.creator.label == 'LinearFunction'):
+                bottom_blob.data = Vutil.invert_linear(v,
+                    guided=self.guided, ignore_bias=self.ignore_bias,
+                    rms=self.fixed_RMS, gamma=self.gamma)
+            # その他(LRN等)
+            else:
+                bottom_blob.data = v.data
+            # 1つ前の層をたどる
+            v = v.creator.inputs[0]
+
+    def get_deconv(self, variable, indices):
+        # 1. 最も活性した場所以外を0にする
+        #maxbounds = self.get_max_patch_bounds(loss, rank, indices)
+        isfc = Vutil.has_fc_layer(variable)
+        # 全結合層の可視化の場合
+        if isfc:
+            values = Vutil.get_fc_info(variable, indices)
+            variable.data.fill(0)
+            for i, (j, v) in enumerate(zip(indices, values)):
+                variable.data[i, j] = v
+        # 畳み込み層やプーリング層などの可視化の場合
+        else:
+            maxinfo = Vutil.get_max_info(variable, indices)
+            variable.data.fill(0)
+            for i, (c, info) in enumerate(zip(indices, maxinfo)):
+                variable.data[i, c, info[1], info[0]] = info[2]
+
+        # 2. 入力層まで逆操作を繰り返す
+        data_layer = Vutil.get_data_layer(variable)
+        xp = cuda.get_array_module(data_layer.data)
+
+        fixed_RMS = 300
+        if xp == cupy:
+            rms = cupy.sqrt(cupy.sum(data_layer.data ** 2, axis=(1,2,3)) / np.product(data_layer.data.shape[1:]))
+            #rms = cupy.sqrt(cupy.sum(convW ** 2, axis=(2, 3)) / np.product(convW.shape[2:]))
+        else:
+            rms = np.linalg.norm(data_layer.data, axis=(1,2,3)) ** 2 / np.product(data_layer.data.shape[1:])
+            #rms = np.linalg.norm(convW, axis=(2, 3)) ** 2 / np.product(convW.shape[2:])
+        scale = fixed_RMS / rms
+        scale = scale.reshape(-1,1,1,1)
+        #print(rms, scale)
+        #data_layer.data *= scale
+
+        self.deconv(variable)
+
+        return data_layer.data
+
+    def __call__(self, trainer):
+        """override method of extensions.Evaluator."""
+        # set up a reporter
+        reporter = reporter_module.Reporter()
+        if hasattr(self, 'name'):
+            prefix = self.name + '/'
+        else:
+            prefix = ''
+        for name, target in six.iteritems(self._targets):
+            reporter.add_observer(prefix + name, target)
+            reporter.add_observers(prefix + name,
+                                   target.namedlinks(skipself=True))
+
+        with reporter:
+            self.deconv_image_dir = os.path.join(trainer.out, 'saliency')
+            if not os.path.exists(self.deconv_image_dir):
+                os.makedirs(self.deconv_image_dir)
+            result, locs, bounds = self.evaluate()
+            if not os.path.exists(trainer.out):
+                os.makedirs(trainer.out)
+            #print(bounds)
+            #ioutil.savetxt(os.path.join(trainer.out, self.layer_name + '.txt'),
+            #                features, delimiter='\t')
+            #cupy.savez(os.path.join(trainer.out, self.layer_name + '.npz'),
+            #                **{self.layer_name: features})
+            '''self.save_tuple_list(os.path.join(trainer.out,
+                        'maxloc_' + self.layer_name + '.txt'), locs)
+            self.save_tuple_list(os.path.join(trainer.out,
+                        'maxbounds_' + self.layer_name + '.txt'), bounds)'''
+        reporter_module.report(result)
+        return result
+
+    def evaluate(self):
+        """override method of extensions.Evaluator."""
+
+        iterator = self._iterators['main']
+        target = self._targets['main']
+        eval_func = self.eval_func or target
+
+        if self.eval_hook:
+            self.eval_hook(self)
+        it = copy.copy(iterator)
+        summary = reporter_module.DictSummary()
+        max_locs = []
+        bounds = []
+        n_processed = 0
+        for batch in it:
+            observation = {}
+            with reporter_module.report_scope(observation):
+                in_arrays = self.converter(batch, self.device)
+                if isinstance(in_arrays, tuple):
+                    in_vars = tuple(variable.Variable(x, volatile='off')
+                                    for x in in_arrays)
+                    eval_func(*in_vars)
+                elif isinstance(in_arrays, dict):
+                    in_vars = {key: variable.Variable(x, volatile='off')
+                               for key, x in six.iteritems(in_arrays)}
+                    eval_func(**in_vars)
+                else:
+                    in_var = variable.Variable(in_arrays, volatile='off')
+                    eval_func(in_var)
+
+            # deconv対象の層のVariableを取得
+            layer_variable = Vutil.get_variable(
+                observation[self.lastname], self.layer_rank)
+
+            pred = self.getpred(layer_variable)
+            truth = in_vars[1].data
+            xp = cuda.get_array_module(pred)
+            if xp == cupy:
+                pred = pred.get()
+                truth = truth.get()
+            indices = pred.flatten() if self.target == 'pred' else truth.flatten()
+
+            # 最大値の位置の計算に必要な入力層の領域を取得
+            isfc = Vutil.has_fc_layer(layer_variable)
+            if isfc:
+                bounds = Vutil.get_data_bounds(layer_variable)
+            else:
+                bounds = Vutil.get_max_bounds(layer_variable, indices)
+            # deconvを実行
+            deconv_data = self.get_deconv(
+                layer_variable, indices)
+
+            topk = np.arange(n_processed, n_processed + len(batch)) // self.n_features
+
+            for i, (t, p, d, b) in enumerate(zip(truth, indices, deconv_data, bounds)):
+                #print(dir(d))
+                # deconvされた入力層に平均画像を足して画像化
+                img = ioutil.deprocess(d.get(), self.mean)
+                # 最大値の位置の計算に必要な入力層の領域だけクロッピングして保存
+                img.crop((b[0], b[2], b[1], b[3])).save(
+                os.path.join(self.deconv_image_dir,
+                    "{0:0>6}_{1:0>4}_{2:0>4}.png".format(n_processed + i, t, p)))
+
+            '''max_locs.extend(self.get_max_locs(
+                observation[self.lastname], self.layer_rank, indices))
+            bounds.extend(self.get_max_patch_bounds(
+                observation[self.lastname], self.layer_rank, indices))'''
+            n_processed += len(batch)
+
+            #self.add_to_confmat(self.confmat, in_vars[1].data, self.getpred(observation[self.lastname]))
+            summary.add(observation)
+        #print(self.confmat)
+        #print(np.diag(self.confmat))
+        #print(1.0 * np.diag(self.confmat).sum() / self.confmat.sum())
+        return summary.compute_mean(), max_locs, bounds
